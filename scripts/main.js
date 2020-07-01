df8ecdd98f61debebd7f7fbabe368e3b42ac97ee
Gustavo <gstonin95@gmail.com>
2020-07-01 17:02:00 +0100

    added logic to click to play and saves board on array

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,56 @@ const createPlayer = function (name) {
 
     return {name, incrementScore, getScore};
 };
+
+
+const gameBoard = (function () {
+    
+    let turnCounter = 0;
+    let playsBoard = [['','',''],['','',''],['','','']];                     /* represents game board - x x x
+                                                                                                        x x x
+                                                                                                        x x x */
+
+    const playableSqs = document.querySelectorAll('.playableSquare');
+    playableSqs.forEach(playableSq => { playableSq.addEventListener('click', (e) => {
+        
+        if (!e.target.textContent) {                                                        //prevents from playing in an already played spot
+            if (turnCounter%2 == 0) {
+                
+                e.target.textContent = 'X';
+                turnCounter++;
+                
+            } else {
+                
+                e.target.textContent = 'O';
+                turnCounter++;
+            }
+
+            arrReference = Number((e.target.id).replace('square',''));                              //saves move on the Arr
+            updatePlaysBoard(arrReference, e);
+
+        }
+        
+        function updatePlaysBoard(reference) {
+            
+            if (reference <= 2) {
+                playsBoard[0][reference%3] = e.target.textContent;
+
+            } else if (reference <= 5) {
+                playsBoard[1][reference%3] = e.target.textContent;
+
+            } else {                                                                                //comes here if its 6, 7 or 8
+                playsBoard[2][reference%3] = e.target.textContent;
+            }
+        }
+
+        });
+    });
+
+
+
+
+    // function renderBoard(arrGame){
+
+    // }
+
+})();
